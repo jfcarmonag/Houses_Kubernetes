34d6f39eea43c474f8b8e8e81d21d377e3cb254b
Juan_Felipe <jfcarmonag@gmail.com>
2019-12-01 18:56:12 -0500

    project finished

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=jfcarmonag/api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run appl --image=jfcarmonag/api --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward appl-7fc7cbc555-pdskv 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,12 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath = jfcarmonag/api
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login --username=jfcarmonag
+docker tag api jfcarmonag/api
 # Step 3:
 # Push image to a docker repository
+docker push jfcarmonag/api
